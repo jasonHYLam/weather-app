2a73f581be7c2f4133f916b53e4727761ff1b58a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-01 14:49:44 +0100

    Create Search Form For Weather At Location

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+// create class for weatherData objects
+
+class WeatherData {
+    constructor(
+        location, 
+        country, 
+        lat, 
+        lon,
+        cloud,
+        condition,
+        tempC, 
+        tempF,
+        gustKph,
+        gustMph,
+        humidity,
+        precipitationInch,
+        precipitationMm,
+        )
+        {
+        this.location = location;
+        this.country = country; 
+        this.lat = lat;
+        this.lon = lon;
+        this.cloud = cloud;
+        this.condition = condition;
+        this.tempC = tempC;
+        this.tempF = tempF;
+        this.gustKph = gustKph;
+        this.gustMph = gustMph;
+        this.humidity = humidity;
+        this.precipitationInch = precipitationInch;
+        this.precipitationMm = precipitationMm;
+        }
+
+}
 // obtain weather data for a location
 async function getWeatherDataForLocation(location) {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=7bfb43be76904809a18182305233007&q=${location}&aqi=no`, {
@@ -7,8 +42,24 @@ async function getWeatherDataForLocation(location) {
     const data = await response.json();
     console.log(data);
 
-    // console.log(data.current.condition.feelslike_c)
-    console.log(data.current.feelslike_c)
+    const weatherDataObject = new WeatherData(
+        data.location.name,
+        data.location.country,
+        data.location.lat,
+        data.location.lon,
+        data.current.cloud,
+        data.current.condition.text,
+        data.current.feelslike_c,
+        data.current.feelslike_f,
+        data.current.gust_kph,
+        data.current.gust_mph,
+        data.current.humidity,
+        data.current.precip_in,
+        data.current.precip_mm,
+    )
+
+    console.log(weatherDataObject);
+
 }
 
-getWeatherDataForLocation('london');
+getWeatherDataForLocation('santa moniz');
